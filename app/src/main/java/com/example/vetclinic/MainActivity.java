655d8cb55b23da0c0f47b24d67e14102bb8a02d1
Polina <poly.gera777@gmail.com>
2023-03-25 14:15:50 +0300

    First commit

---
+++ app/src/main/java/com/example/vetclinic/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.vetclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vetclinic.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, MyProfileFragment.class, null)
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/vetclinic/MyProfileFragment.java
@@ -0,0 +1,23 @@
+package com.example.vetclinic;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vetclinic.databinding.ActivityMainBinding;
+
+
+public class MyProfileFragment extends Fragment {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
