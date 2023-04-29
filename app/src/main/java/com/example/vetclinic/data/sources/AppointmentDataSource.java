782ee425e3abf3a9336878356430a2434577f7eb
Polina <poly.gera777@gmail.com>
2023-04-29 06:27:41 +0300

    Завершение работы

--- app/src/main/java/com/example/vetclinic/data/sources/AppointmentDataSource.java
+++ app/src/main/java/com/example/vetclinic/data/sources/AppointmentDataSource.java
@@ -6,10 +6,13 @@ import androidx.lifecycle.Observer;
 
 public class AppointmentDataSource {
 
-
     public static String getAppointmentsName(int randInt) {
         return null;
     }
-
+    /*
+    private static final String appointmentsNames[] = {"Собака", "Кошка", "Птица"};
+    public static String getAppointmentsName(int randInt){
+        return appointmentsNames[randInt];
+    }*/
 
 }
